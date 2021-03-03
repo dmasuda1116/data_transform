6bece4e4e38be6b41e34577f2fb93edc69aef12f
Joy <76196211+Hikaru-Furuta@users.noreply.github.com>
2021-03-03 15:34:48 +0900

    This is a script to binarize data in a csv
    Usage:
    python editing_csv.py -i (input csv file name) -o (output csv file name)

--- editing.py
+++ editing.py
@@ -1,5 +1,5 @@
 """
-This script is for converting csv and modifying data to quantify features and to interpolate missing values.
+This script binarizes the data in a csv.
 
 """
 # coding: utf-8
@@ -9,6 +9,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import itertools
 import csv
+import sys
+import argparse
+
 print('         .       ')
 print('         .       ')
 print(' Loading the csv ')
@@ -16,55 +19,97 @@ print('         .       ')
 print('         .       ')
 
 #Reading a csv file
+parser = argparse.ArgumentParser('Files')
+parser.add_argument('-i','--file')
+parser.add_argument('-o','--output')
+f_args = parser.parse_args()
+input_csv = f_args.file
+output_csv = f_args.output
+print('in:',input_csv)
+print('out:',output_csv)
+def Binarization(input_csv, output_csv):
+    data = pd.read_csv(input_csv, encoding='Shift-JIS').values.tolist()
+    print('==========================')
 
-data = pd.read_csv('ingredient.csv', encoding='Shift-JIS').values.tolist()
-print('==========================')
+    print('#Loaded data from the csv#')
+    print('--------------------------')
 
-print('#Loaded data from the csv#')
-print('--------------------------')
+    print(data)
+    print('==========================')
+    ingre_sep=[]
 
-print(data)
-print('==========================')
-ingre_sep=[]
+    for i in range(len(data)):
+        ingre_sep.append(data[i][1].split('、'))
 
-for i in range(len(data)):
-    ingre_sep.append(data[i][1].split('、'))
+    ingre_sep_f=list(itertools.chain.from_iterable(ingre_sep))
+    ingre_unique_list=(np.unique(ingre_sep_f)).tolist()
 
-ingre_sep_f=list(itertools.chain.from_iterable(ingre_sep))
-ingre_unique_list=(np.unique(ingre_sep_f)).tolist()
+    print('         .       ')
+    print('         .       ')
+    print('Modifying the data')
+    print('         .       ')
+    print('         .       ')
 
-print('         .       ')
-print('         .       ')
-print('Modifying the data')
-print('         .       ')
-print('         .       ')
+    modified_data=[]
+    #Giving the variables to modified_data which is to be converted in a csv file
+    variables = ['name']+ingre_unique_list
+    modified_data.append(variables)
+    #Setting values for each column, 'j'th one.
+    for j in range(len(data)):
+        renewed_data=[]
+        renewed_data.append(data[j][0])
+        for i in range(len(ingre_unique_list)):
+            #converting imformation whether each ingredient is included in the product or not, 0 or 1, so called binarization.
 
-modified_data=[]
-#Giving the variables to modified_data which is to be converted in a csv file
-variables = ['name']+ingre_unique_list
-modified_data.append(variables)
-#Setting values for each column, 'j'th one.
-for j in range(len(data)):
-    renewed_data=[]
-    renewed_data.append(data[j][0])
-    for i in range(len(ingre_unique_list)):
-        #converting imformation whether each ingredient is included in the product or not, 0 or 1, so called bidernization.
+            if ingre_unique_list[i] in ingre_sep[j]:
+                renewed_data.append(1)
+            else:
+                renewed_data.append(0)
+        modified_data.append(renewed_data)
 
-        if ingre_unique_list[i] in ingre_sep[j]:
-            renewed_data.append(1)
-        else:
-            renewed_data.append(0)
-    modified_data.append(renewed_data)
+    print('==========================')
 
-print('==========================')
+    print('#Data-modifying completed#')
+    print('--------------------------')
 
-print('#Data-modifying completed#')
-print('--------------------------')
+    print(modified_data)
+    print('==========================')
+    input_csv
+    with open(output_csv, 'w', encoding='Shift-JIS') as f:
+        for i in range(len(modified_data)):
+            writer = csv.writer(f, lineterminator='\n')
+            writer.writerow(modified_data[i])
+    print('         .       ')
+    print('         .       ')
+    print('--------------------------')
+    print('#You have obtained ['+ output_csv +'] in the current directory')
+    print('--------------------------')
 
-print(modified_data)
-print('==========================')
+    return pd.read_csv(output_csv, encoding='Shift-JIS').values.tolist()
 
-for i in range(len(modified_data)):
-    with open('ingredient_mod.csv', 'w', encoding='Shift-JIS') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(modified_data[i])
+
+if (not input_csv) or (not output_csv):
+    print('========================================================================================================')
+    print('==========================================-------NOTICE-------==========================================')
+    print('')
+    print(' Usage example for this script : python editing_csv.py -i (input csv file name) -o (output csv file name)')
+    print('')
+    print('========================================================================================================')
+
+else:
+    print('=========================')
+    print('~Do you want to select files?~')
+    print('=========================')
+
+    print('a: Yes')
+    print('b: No, files are already choosen')
+    mode = input("a or b:")
+    if mode == 'a':
+        input_f = input("Choose the file to convert:")
+        output_f = input("Name the file to export:")
+    elif mode == 'b':
+        pass
+    else:
+        print('invalid answer')
+        exit(0)
+Bidernization(input_csv, output_csv)
